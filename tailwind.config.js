7eed8d358d33083ada3494bdc82c15503cade4cc
Kris Fragata <kristinnefragata@outlook.com>
2024-02-26 14:05:56 -0500

    fixing colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,13 @@ export default {
 		extend: {
 			colors: {
 				logoOrange1: '#ff8552',
-				logoOrange: '#B26801',
+				logoOrange: '#9B5C03',
 				highlight: '#204b4f',
 				backgroundBlue: '#a1c3c9',
 				background1: '#e6d3b5',
 				background2: '#043B48',
-				background: '#F7E7DE',
+				background3: '#F7E7DE',
+				background: '#ECF0F1',
 				important1: '#EAC6A5',
 				important: '#915D03',
 				paragraph: '#043B48'
