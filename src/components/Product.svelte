ff9f0f85527534c273ee06a88e13080ecea3628c
Kris Fragata <kristinnefragata@outlook.com>
2024-02-27 19:23:17 -0500

    changing titles

--- src/components/Product.svelte
+++ src/components/Product.svelte
@@ -9,6 +9,7 @@
 	<div
 		class="bg-backgroundBlue flex flex-1 flex-col gap-5 sm:gap-9 md:gap-14 flex-1 items-center justify-center pb-10 md:pb-14 w-full"
 	>
+		<h1 class="text-7xl font-semibold text-center text-important pt-10 logo">Demo</h1>
 		<div class="flex flex-col items-center w-full">
 			<img
 				src={gif}
